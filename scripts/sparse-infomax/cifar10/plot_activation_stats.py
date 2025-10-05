b37afde753cb000f1732966d0bf0c57340bb8d52
fraiori0 <fraiori@hotmail.it>
2025-10-05 12:03:00 +0200

    result plots for fashionmnist and cifar10

--- scripts/sparse-infomax/cifar10/plot_activation_stats.py
+++ scripts/sparse-infomax/cifar10/plot_activation_stats.py
@@ -11,13 +11,18 @@ from plotly.subplots import make_subplots
 from tqdm import tqdm
 from sklearn.svm import LinearSVC
 from functools import partial
+import yaml
+import plotly.io as pio
 
 np.set_printoptions(precision=4, suppress=True)
+pio.renderers.default = "browser"
 
 def logand_name(eps: str = ""):
+    return r"$\mathcal{L}_{\epsilon}$"
     return r"$\text{MI} \left( \log \langle y_1, y_2 \rangle + \epsilon \right)$ " + eps
 
 def and_name(alpha: str = ""):
+    return r"$\mathcal{L}_{\alpha}$"
     return r"$\text{MI} \left( \langle y_1, y_2 \rangle \right) + \alpha L_1(y_1)$ " + alpha
 
 models = {
@@ -44,6 +49,39 @@ base_folder = os.path.join(
 )
 base_folder = os.path.normpath(base_folder)
 
+save_folder = os.path.join(
+    base_folder,
+    "resources",
+    "results",
+    "cifar10",
+)
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+
+
+fig_layout_double_subplot = {
+    "height": 500,
+    "width": 1000,
+}
+fig_layout_single = {
+    "height": 500,
+    "width": 700,   
+}
+
+
+k_types_and_subtitles = (
+    ("vgg_gavg_sigmoid_logand", "vgg_gavg_sigmoid_and"),
+    (logand_name(), and_name()),
+)
+
+# path to the file with stored result for accuracy, computing them everytime takes a lot of time
+result_path = os.path.join(
+    base_folder,
+    "resources",
+    "results",
+    "cifar10",
+    "acc_vs_sparsity.yaml",
+)
 
 """---------------------"""
 """ Import activation data """
@@ -77,10 +115,31 @@ for k in models.keys():
 """ Plot PER-UNIT distribution of average activity """
 """---------------------"""
 
-k_types_and_subtitles = (
-    ("vgg_gavg_sigmoid_logand", "vgg_gavg_sigmoid_and"),
-    (logand_name(), and_name()),
-)
+
+# First, compute manual bins for the histograms
+# Indeed, a lot of units in the "and" models are exactly or almost 0, so we would like more bins there
+per_unit_activity_bins = {}
+low_exponent = -4
+max_bin = 0
+min_bin_center = (10**low_exponent)*0.7
+for i, k in enumerate(k_types_and_subtitles[0]):
+    per_unit_activity_bins[k] = {}
+    for kk in models[k].keys():
+        
+        avg_act = models[k][kk]["data"]["zs"].mean(axis=0)
+
+        # take the maximum avg_act
+        max_avg_act = np.max(avg_act)
+
+        # create 50 bins in logarithmic scale from 1e-4 to max_avg_act
+        bins = np.logspace(low_exponent, np.log10(max_avg_act+1e-4), 50)
+        # pre-pend an initial 0
+        bins = np.concatenate((np.zeros(1), bins))
+
+        per_unit_activity_bins[k][kk] = bins
+
+        if max_avg_act > max_bin:
+            max_bin = max_avg_act+1e-4
 
 
 fig = make_subplots(
@@ -95,30 +154,63 @@ for i, k in enumerate(k_types_and_subtitles[0]):
 
         print(avg_act.shape)
 
+        # fig.add_trace(
+        #     go.Histogram(
+        #         x=avg_act,
+        #         name=models[k][kk]["name"],
+        #         opacity=0.5,
+        #         # nbinsx=100,
+        #         # histnorm="probability",
+        #         # marker_color=models[k][kk]["color"],
+        #         legend=f"legend{i+1}",
+        #         xbins=dict(
+        #             start=per_unit_activity_bins[k][kk][0],
+        #             end=per_unit_activity_bins[k][kk][-1],
+        #             size=(per_unit_activity_bins[k][kk][1:] - per_unit_activity_bins[k][kk][:-1]),
+        #         ),
+        #     ),
+        #     row=1,
+        #     col=i+1,
+        # )
+        
+        
+        edge_centers = 0.5 * (per_unit_activity_bins[k][kk][1:] + per_unit_activity_bins[k][kk][:-1])
+        edges = per_unit_activity_bins[k][kk]
+        counts, _ = np.histogram(avg_act, bins=edges)
+        # normalize counts to sum to 1
+        counts = counts / np.sum(counts)
+
         fig.add_trace(
-            go.Histogram(
-                x=avg_act,
+            go.Bar(
+                x=edge_centers,
+                y=counts,
                 name=models[k][kk]["name"],
                 opacity=0.5,
-                nbinsx=50,
-                histnorm="probability",
+                # nbinsx=100,
+                # histnorm="probability",
                 # marker_color=models[k][kk]["color"],
-                legend=f"legend{i+1}"
+                legend=f"legend{i+1}",
+                width=(edges[1:] - edges[:-1]),
             ),
             row=1,
             col=i+1,
-        )
+        )            
 
     # Set subplot title font
     fig.layout.annotations[i].font = dict(size=18, family="Times New Roman")
     # Set font and axis titles
     fig.update_xaxes(
         title=dict(
-            text=r"Average activity",
+            text=r"Average activity (log-scale)",
             font=dict(size=18, family="Times New Roman")),
         row=1,
         col=i+1,
         tickfont=dict(size=16, family="Times New Roman"),
+        type="log",
+        # set range explicitly
+        range=[np.log10(min_bin_center), np.log10(1.01)],
+        # add ticks at powers of 10
+        tickvals=[10**i for i in range(low_exponent, 1)],
     )
     fig.update_yaxes(
         title=dict(
@@ -137,17 +229,32 @@ fig.update_layout(
     # title_text=r"Per-unit distribution of average activity",
     barmode="overlay",
     # legend=dict(x=0.01, y=0.99),
-    width=1200,
-    height=600,
+    **fig_layout_double_subplot,
     template="plotly_white",
 )
 fig.show()
 
+# save figure as pdf
+fig.write_image(os.path.join(save_folder, "per_unit_activity.pdf"), format="pdf", scale=3, **fig_layout_double_subplot)
 
 # exit()
 """---------------------"""
 """ Plot PER-SAMPLE distribution of average activity """
 """---------------------"""
+
+# First, compute the maximum, so we know how to set axis range for both plot (so they have same range)
+
+
+max_activation_per_sample = 0
+for i, k in enumerate(k_types_and_subtitles[0]):
+    per_unit_activity_bins[k] = {}
+    for kk in models[k].keys():
+        
+        avg_act = models[k][kk]["data"]["zs"].mean(axis=-1)
+        max_avg_act = np.max(avg_act)
+
+        if max_avg_act > max_activation_per_sample:
+            max_activation_per_sample = max_avg_act+1e-4
 
 
 fig = make_subplots(
@@ -186,6 +293,8 @@ for i, k in enumerate(k_types_and_subtitles[0]):
         row=1,
         col=i+1,
         tickfont=dict(size=16, family="Times New Roman"),
+        # set range manually
+        range=[0.0, max_activation_per_sample],
     )
     fig.update_yaxes(
         title=dict(
@@ -203,80 +312,100 @@ for i, k in enumerate(k_types_and_subtitles[0]):
 fig.update_layout(
     # title_text=r"Per-unit distribution of average activity",
     barmode="overlay",
-    legend=dict(x=0.01, y=0.99),
-    width=1200,
-    height=600,
+    **fig_layout_double_subplot,
     template="plotly_white",
 )
 fig.show()
+
+# save figure as pdf
+fig.write_image(os.path.join(save_folder, "per_sample_activity.pdf"), format="pdf", scale=3, **fig_layout_double_subplot)
 
 
 """---------------------"""
 """ Classification accuracy with varying level of sparsification """
 """---------------------"""
 
-N_K = [256, 25, 20, 15, 10, 5]
-
-# For each model, train a linear SVM after sparsifying the activations by keeping only the k highest activations per sample
 
 def keep_top_k(x, k):
-    """ Keep only the k highest activations per sample, set rest to 0 """
-    # x: (n_samples, n_features)
-    # output: (n_samples, n_features)
-    def keep_top_k_single(x_single, k):
-        thresh = np.partition(x_single, -k)[-k]
-        return np.where(x_single >= thresh, x_single, 0.0)
-    # vmap over samples
-    return jax.vmap(partial(keep_top_k_single, k=k))(x)
+        """ Keep only the k highest activations per sample, set rest to 0 """
+        # x: (n_samples, n_features)
+        # output: (n_samples, n_features)
+        def keep_top_k_single(x_single, k):
+            thresh = np.partition(x_single, -k)[-k]
+            return np.where(x_single >= thresh, x_single, 0.0)
+        # vmap over samples
+        return jax.vmap(partial(keep_top_k_single, k=k))(x)
 
+# first, check if a file with the stored results is already present
+if os.path.exists(result_path):
+    
+    print(f"Loading results from {result_path}")
+    with open(result_path, "r") as f:
+        accuracy_results = yaml.safe_load(f)
 
-# NOTE, this nested cycles takes a while to run
-for n_top_k in tqdm(N_K):
+    # Add it to the models dict
+    N_K = accuracy_results["n_top_k"]
+    for k in models.keys():
+        for kk in models[k].keys():
+            models[k][kk]["svm_acc"] = {}
+            for i_top_k, n_top_k in enumerate(N_K):
+                models[k][kk]["svm_acc"][n_top_k] = {
+                    "train": accuracy_results[k][kk]["train"][i_top_k],
+                    "val": accuracy_results[k][kk]["val"][i_top_k],
+                }
+else:
 
-    sparsity = 1.0 - n_top_k/256.0
-    keep_top_k_jitted = jax.jit(partial(keep_top_k, k=n_top_k))
+    N_K = [256, 25, 20, 15, 10, 5]
 
-    for k in tqdm(models.keys(), total=len(models.keys()), leave=False):
-        for kk in tqdm(models[k].keys(), total=len(models[k].keys()), leave=False):
-            
-            take_first = 20  # to speed up the process for debugging
-            
-            zs = models[k][kk]["data"]["zs"].copy()#[:take_first]
-            zs_val = models[k][kk]["data"]["zs_val"].copy()#[:take_first]
-            labels_onehot = models[k][kk]["data"]["labels_onehot"].copy()#[:take_first]
-            labels_onehot_val = models[k][kk]["data"]["labels_onehot_val"].copy()#[:take_first]
+    # For each model, train a linear SVM after sparsifying the activations by keeping only the k highest activations per sample
 
-            labels_categorical = labels_onehot.argmax(axis=-1)
-            labels_categorical_val = labels_onehot_val.argmax(axis=-1)
+    # NOTE, this nested cycles takes a while to run
+    for n_top_k in tqdm(N_K):
 
-            if sparsity > 0.1:
-                zs = keep_top_k_jitted(zs)
-                zs_val = keep_top_k_jitted(zs_val)
+        sparsity = 1.0 - n_top_k/256.0
+        keep_top_k_jitted = jax.jit(partial(keep_top_k, k=n_top_k))
 
-            svm_model = LinearSVC(
-                random_state=0,
-                tol=1e-4,
-                multi_class="ovr",
-                intercept_scaling=1,
-                C=8,
-                penalty="l1",
-                loss="squared_hinge",
-                max_iter=2000,
-            )
+        for k in tqdm(models.keys(), total=len(models.keys()), leave=False):
+            for kk in tqdm(models[k].keys(), total=len(models[k].keys()), leave=False):
+                
+                take_first = 20  # to speed up the process for debugging
+                
+                zs = models[k][kk]["data"]["zs"].copy()#[:take_first]
+                zs_val = models[k][kk]["data"]["zs_val"].copy()#[:take_first]
+                labels_onehot = models[k][kk]["data"]["labels_onehot"].copy()#[:take_first]
+                labels_onehot_val = models[k][kk]["data"]["labels_onehot_val"].copy()#[:take_first]
 
-            svm_model.fit(
-                zs,
-                labels_categorical,
-            )
-            acc_train = svm_model.score(zs, labels_categorical)
-            acc_val = svm_model.score(zs_val, labels_categorical_val)
+                labels_categorical = labels_onehot.argmax(axis=-1)
+                labels_categorical_val = labels_onehot_val.argmax(axis=-1)
 
-            models[k][kk].setdefault("svm_acc", {})
+                if sparsity > 0.1:
+                    zs = keep_top_k_jitted(zs)
+                    zs_val = keep_top_k_jitted(zs_val)
 
-            models[k][kk]["svm_acc"][n_top_k] = {
-                "train": acc_train,
-                "val": acc_val,
-            }
+                svm_model = LinearSVC(
+                    random_state=0,
+                    tol=1e-4,
+                    multi_class="ovr",
+                    intercept_scaling=1,
+                    C=8,
+                    penalty="l1",
+                    loss="squared_hinge",
+                    max_iter=2000,
+                )
+
+                svm_model.fit(
+                    zs,
+                    labels_categorical,
+                )
+                acc_train = svm_model.score(zs, labels_categorical)
+                acc_val = svm_model.score(zs_val, labels_categorical_val)
+
+                models[k][kk].setdefault("svm_acc", {})
+
+                models[k][kk]["svm_acc"][n_top_k] = {
+                    "train": acc_train,
+                    "val": acc_val,
+                }
 
 for k in models.keys():
     for kk in models[k].keys():
@@ -295,7 +424,7 @@ for k in models.keys():
 fig = go.Figure()
 sparsities = 1.0 - np.array(N_K)/256.0
 log_sparsities = 25.0**sparsities
-for k in models.keys():
+for i, k in enumerate(models.keys()):
     for kk in models[k].keys():
         
         accs_val = np.array([models[k][kk]["svm_acc"][n_top_k]["val"] for n_top_k in N_K])
@@ -315,8 +444,15 @@ for k in models.keys():
                     symbol=models[k][kk]["symbol"],
                     size=10,
                 ),
+                legend=f"legend{i+1}"
             )
         )
+
+    # place the legend
+    fig.update_layout(
+        **{f"legend{i+1}":dict(x=1.1, y=(0.98 - i*0.3), xanchor='left', yanchor='top', title_text=k_types_and_subtitles[1][i]),}
+    )
+
 
 # Set font and axis titles
 fig.update_xaxes(
@@ -340,9 +476,12 @@ fig.update_yaxes(
 fig.update_layout(
     # title_text=r"Classification accuracy vs. sparsification",
     # legend=dict(x=0.01, y=0.99),
-    width=700,
-    height=500,
+    **fig_layout_single,
     template="plotly_white",
 )
 
 fig.show()
+
+
+# save figure as pdf
+fig.write_image(os.path.join(save_folder, "accuracy_vs_sparsity.pdf"), format="pdf", scale=3, **fig_layout_single)
